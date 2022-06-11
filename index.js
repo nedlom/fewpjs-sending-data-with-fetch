67e4295248be7ac3ec8119d68d58fb4fb3a527bd
Nick <nelom3@gmail.com>
2022-06-11 16:23:03 -0400

    commit tree d4ab7689d9cb7ec2981b43154d58350ac4adf56d

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+
+function submitData(name, email) {
+
+  const formData = {
+    name: name,
+    email: email
+  };
+  
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+  
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      const h1 = document.createElement('h1');
+      h1.innerHTML = object.id;
+      document.body.appendChild(h1)
+    }).catch(function(error) {
+      const h1 = document.createElement('h1');
+      h1.innerHTML = error.message;
+      document.body.appendChild(h1)
+    });;
+
+}
